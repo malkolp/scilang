163e3799b299a09e4d988a01dffaa574324af738
malkolp <mf64849@gmail.com>
2019-12-01 02:26:02 -0500

    grammar processing

--- src/tool/component/syntactic/Grammar.java
+++ src/tool/component/syntactic/Grammar.java
@@ -1,4 +1,25 @@
 package tool.component.syntactic;
 
 public class Grammar {
+
+    private String key;
+    private double value,precedence;
+
+    public Grammar(String key, double value, double precedence){
+        this.key = key;
+        this.value = value;
+        this.precedence = precedence;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public double getPrecedence() {
+        return precedence;
+    }
 }
